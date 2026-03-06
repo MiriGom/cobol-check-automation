13713fcfafd0bd16325de056a41502f97e9eb15d
MiriGom <miriamalejandrago@gmail.com>
2026-03-06 07:37:10 -0500

    Refactor zowe_operations.sh for directory handling

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -2,18 +2,20 @@
 # zowe_operations.sh
 
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
-COBOL_CHECK_FOLDER="cobol-check-0.2.19"
 
-# Check directory
+# Check if directory exists, create if it doesn't
 if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
-  echo "Creating directory..."
-  zowe zos-files create uss-directory "/z/$LOWERCASE_USERNAME/cobolcheck"
+  echo "Directory does not exist. Creating it..."
+  zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
 else
-  echo "Directory exists."
+  echo "Directory already exists."
 fi
 
 # Upload files
-zowe zos-files upload dir-to-uss "./$COBOL_CHECK_FOLDER" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "*.jar"
+zowe zos-files upload dir-to-uss "./cobol-check-0.2.19" "/z/$LOWERCASE_USERNAME/cobolcheck" \
+  --recursive \
+  --binary-files "cobol-check-0.2.19.jar"
 
 # Verify upload
+echo "Verifying upload:"
 zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
